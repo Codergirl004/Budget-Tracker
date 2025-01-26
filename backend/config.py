eb5c1f9f57135b8424566264846fecd52f1eaa86
Riya Thachett <rthachett@gmail.com>
2025-01-26 10:48:02 +0530

    finale

--- backend/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'Success@123'  # Secret key for session management, etc.
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'  # SQLite database URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable SQLAlchemy track modifications for performance
